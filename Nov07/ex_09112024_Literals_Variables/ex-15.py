c4cd90a8486ef586f227361ff6b38513cdedb21d
deeya96 <hkds3123@gmail.com>
2024-12-05 13:02:37 +0530

    commit

---
+++ Nov07/ex_09112024_Literals_Variables/ex-15.py
@@ -0,0 +1,11 @@
+#logic building
+num1 = int(input("enter number 1"))
+num2 = int(input("enter number 2"))
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("sum is is :", sum)
+print("sub is :", sub)
+print("mul is :", mul)
+print("div is :", div)
