64de56438b23b31cbcf77c3cc02c48c5eed85bf8
deeya96 <hkds3123@gmail.com>
2024-12-18 21:02:40 +0530

    commit

---
+++ Nov07/ex_09112024_Literals_Variables/nov-9-Task-1.py
@@ -0,0 +1,18 @@
+# task1 : take 2 numbers as user input and perform basic mathematical calculations
+# task 2: take 2 user-input and print quotient and reminder
+a = int(input("enter number 1"))
+b = int(input("enter number 2"))
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("sum is is :", sum)
+print("sub is :", sub)
+print("mul is :", mul)
+print("div is :", div)
+# Quotient
+q = a // b  # double division operator
+print("Quotient:", q)
+# remainder
+r = a % b
+print("remainder:", r)
